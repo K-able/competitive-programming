f93788368f7892fd6d5556133aea65e34d1771a8
K-able <i.kable1.1@gmail.com>
2020-04-12 20:56:33 +0900

    update

--- past-contest/ABC156/d.py
+++ past-contest/ABC156/d.py
@@ -1,7 +1,15 @@
-from scipy.special import comb
+mod = 10**9+7
+
+def calculate_combination(n,k):
+    x, y = 1, 1
+    for i in range(k):
+        x *= (n - i) % mod
+        y *= (k - i) % mod
+    return x * pow(y, mod-2, mod) % mod
 
 n, a, b = map(int, input().split())
-ans = (pow(2, n) - 1) % (10**9+7)
-#ans -= (comb(n,a) + comb(n,b))
-#ans %= (10**9+7)
-print(int(ans))
+
+ans = pow(2, n, mod) - 1 - calculate_combination(n,a) - calculate_combination(n,b)
+while ans < 0:
+    ans += mod
+print(ans)
