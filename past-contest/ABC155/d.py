838d35c421e8710a54f040ec4cd466791cda9042
K-able <i.kable1.1@gmail.com>
2020-04-26 01:58:36 +0900

    update

--- past-contest/ABC155/d.py
+++ past-contest/ABC155/d.py
@@ -1,14 +1,35 @@
-'''
-n = int(input())
-s_dic = {}
-for i in range(n):
-    s = input()
-    if s not in s_dic:
-        s_dic[s] = 1
+import numpy as np
+
+n, k = map(int, input().split()) 
+a_list = np.array(list(map(int, input().split())))
+
+a_list.sort()
+
+zero_list = a_list[a_list == 0]
+minus_list = a_list[a_list < 0]
+plus_list = a_list[a_list > 0]
+
+def index_count(x):
+    cnt = 0
+    if x >= 0:
+        cnt += len(zero_list) * n
+    cnt += np.searchsorted(a_list, x // plus_list, side='right').sum()
+    cnt += (n - np.searchsorted(a_list, -(-x // minus_list), side='left')).sum()
+    cnt -= np.count_nonzero(a_list * a_list <= x)
+ 
+    assert cnt % 2 == 0
+    return cnt // 2
+ 
+ 
+left = -10**18
+right = 10**18
+ 
+while left + 1 < right:
+    mid = (left + right) // 2
+    a = index_count(mid)
+    if a >= k:
+        right = mid
     else:
-        s_dic[s] += 1
-max_s_list = [s[0] for s in s_dic.items() if s[1] == max(s_dic.values())]
-max_s_list.sort()
-for s in max_s_list:
-    print(s)
-'''
+        left = mid
+ 
+print(right)
