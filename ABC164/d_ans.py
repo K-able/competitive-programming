6366591648b7776ba15c37077ad71bfb67276f58
K-able <i.kable1.1@gmail.com>
2020-05-09 14:13:19 +0900

    update

---
+++ ABC164/d_ans.py
@@ -0,0 +1,18 @@
+s = input()[::-1]
+
+counts = [0] * 2019
+counts[0] = 1
+
+num, d = 0, 1
+
+for char in s:
+    num += int(char) * d
+    num %= 2019
+    d *= 10
+    d %= 2019
+    counts[num] += 1
+
+ans = 0
+for cnt in counts:
+    ans += cnt * (cnt - 1) // 2
+print(ans)
