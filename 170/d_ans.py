b35b0c05b50cc62f99c89a89fe2fb4033eea3449
K-able <i.kable1.1@gmail.com>
2020-06-30 16:45:59 +0900

    update

--- 170/d_ans.py
+++ 170/d_ans.py
@@ -1,36 +1,21 @@
-def quick_sort(arr):
-    left = []
-    right = []
-    if len(arr) <= 1:
-        return arr
-    ref = arr[0]
-    ref_count = 0
-    for ele in arr:
-        if ele < ref:
-            left.append(ele)
-        elif ele > ref:
-            right.append(ele)
-        else:
-            ref_count += 1
-    left = quick_sort(left)
-    right = quick_sort(right)
-    return right + [ref] * ref_count + left
+from numba import njit
 
 N = int(input())
 A = list(map(int, input().split()))
+maxA = max(A)
+dp = [0] * maxA
 
-A = [x for x in A if A.count(x) == 1]
-A = quick_sort(A)
+for i in range(N):
+    if dp[A[i]-1] != 0:
+        dp[A[i]-1] = 2
+        continue
+    else:
+        for j in range(1, maxA//A[i]+1):
+            dp[A[i]*j-1] += 1
 
 ans = 0
-if len(A) == 0:
-    print(ans)
-    exit()
 for i in range(N):
-    flag = True
-    for j in range(i+1, N):
-        if A[i] % A[j] == 0:
-            flag = False
-            break
-    if flag: ans += 1
+    if dp[A[i]-1] == 1:
+        ans += 1
+
 print(ans)
