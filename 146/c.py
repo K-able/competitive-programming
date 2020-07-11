ca1b00b7847aeb7203e3776cd02e49b5762e21fc
K-able <i.kable1.1@gmail.com>
2020-07-11 22:24:14 +0900

    update

---
+++ 146/c.py
@@ -0,0 +1,11 @@
+A, B, X = map(int, input().split())
+
+low = 0
+high = 10**9+1
+while high - low > 1:
+    mid = (low + high) // 2
+    if A * mid + B * len(str(mid)) <= X:
+        low = mid
+    else:
+        high = mid
+print(low)
